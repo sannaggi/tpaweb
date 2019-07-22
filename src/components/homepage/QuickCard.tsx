632c5025a6cf919e660ffd32cb3404fef7e81f0f
xyon <albion0211@gmail.com>
2019-07-22 17:23:02 +0700

    added quickcard

--- src/components/homepage/QuickCard.tsx
+++ src/components/homepage/QuickCard.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import '../../css/homepage/quickcard.css'
 
 function QuickCard() {
     return (
-        <div>
-            
+        <div className="quickCard">
+            <div className="quickCardTitle">Book unique places to stay and things to do.</div>
+            <div className="input">
+                <div className="title">WHERE</div>
+                <input type="text" name="" id="" placeholder="Anywhere"/>
+            </div>
+            <div className="input half">
+                <div className="title">CHECK-IN</div>
+                <input type="text" name="" id="" placeholder="mm/dd/yy"/>
+            </div>
+            <div className="input half">
+                <div className="title">CHECK-OUT</div>
+                <input type="text" name="" id="" placeholder="mm/dd/yy"/>
+            </div>
+            <div className="input">
+                <div className="title">GUESTS</div>
+                <input type="text" name="" id="" placeholder="Guests"/>
+            </div>
+            <input type="submit" value="Search"/>
         </div>
     )
 }
