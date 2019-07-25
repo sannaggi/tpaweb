1199c2dfa6b494c365749eaa0c9c64d81b85fd3d
xyon <albion0211@gmail.com>
2019-07-25 14:30:14 +0700

    test

--- src/components/place detail/BannerImage.tsx
+++ src/components/place detail/BannerImage.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactImageAppear from "react-image-appear";
 
 export default function BannerImage(props: any){
 
     return(
-        <ReactImageAppear src={process.env.PUBLIC_URL + props.url}/>
+        <img src={process.env.PUBLIC_URL + props.url} alt={props.alt}/>
     )
 }
