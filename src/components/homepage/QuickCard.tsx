ba10435729607e8ab3e5de22079afe60e2f5dcd9
xyon <albion0211@gmail.com>
2019-07-29 00:45:41 +0700

    added search

--- src/components/homepage/QuickCard.tsx
+++ src/components/homepage/QuickCard.tsx
@@ -3,6 +3,7 @@ import '../../css/homepage/quickcard.css'
 import { connect } from 'react-redux';
 import { newPost } from '../../actions/bookingActions'
 import GuestDropdown from '../layouts/GuestDropdown'
+import axios from 'axios'
 
 function QuickCard({guestCount} : {guestCount:Object}) {
 
@@ -13,6 +14,8 @@ function QuickCard({guestCount} : {guestCount:Object}) {
         guests: {}
     })
 
+    const [searchResult, setSearchResult] = useState([])
+
     useEffect(() => {
         newBooking.guests = guestCount
     }, [guestCount, newBooking.guests])
@@ -21,12 +24,49 @@ function QuickCard({guestCount} : {guestCount:Object}) {
         setNewBooking({...newBooking, [e.target.name] : e.target.value});
     }
 
+    async function takeData(res) {
+        return new Promise((resolve) => {
+            resolve(res.data)
+        })
+    }
+
+    function getLogo(type:string) {
+        if(type === "place") return <span><i className="fa fa-home"></i></span>
+        return  <span>&#10070;</span>
+    }
+
+    function search(e:any) {
+        if(e.target.value === "") {
+            setSearchResult([])
+            return
+        }
+        axios.get("https://aivbnbapi.herokuapp.com/api/search/" + e.target.value, {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(takeData)
+        .then((datas:Array<any>) => {
+            if(datas === null || datas === undefined) {
+                setSearchResult([<li key="xxxxx">No matching data found</li>])
+                return
+            }
+            setSearchResult(datas.map((data) => (
+                <li key={data.id}>{getLogo(data.type)} <div>{data.name} - {data.type}</div></li>
+            )))
+        })
+    }
+
     return (
         <div className="quickCard">
             <div className="quickCardTitle">Book unique places to stay and things to do.</div>
             <div className="input">
                 <div className="title">WHERE</div>
-                <input type="text" name="place" value={newBooking.place} onChange={onChange} placeholder="Anywhere"/>
+                <input type="search" name="place" onChange={search} placeholder="Anywhere"/>
+                <ul className="searchContainer">
+                    {searchResult}
+                </ul>
             </div>
             <div className="input half">
                 <div className="title">CHECK-IN</div>
