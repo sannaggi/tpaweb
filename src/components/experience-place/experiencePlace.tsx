13eab6657884f53c1ab9e41dd0704daaf2a515c3
Yumazine <yumazine@gmail.com>
2019-07-29 12:19:10 +0700

    added stories responsive

--- src/components/experience-place/experiencePlace.tsx
+++ src/components/experience-place/experiencePlace.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import Sharing from "../reusable/sharing";
 import "../../css/experience-place/experiencePlace.css";
 import GoogleMapReact from "google-map-react";
+import InstaStories from "../reusable/InstaStories";
+import Stories from 'react-insta-stories'
+
+const stories = [{ url: 'https://picsum.photos/1080/1920' }, { url: 'https://fsa.zobj.net/crop.php?r=dyJ08vhfPsUL3UkJ2aFaLo1LK5lhjA_5o6qEmWe7CW6P4bdk5Se2tYqxc8M3tcgYCwKp0IAyf0cmw9yCmOviFYb5JteeZgYClrug_bvSGgQxKGEUjH9H3s7PS9fQa3rpK3DN3nx-qA-mf6XN' }, { url: 'https://media.idownloadblog.com/wp-content/uploads/2016/04/iPhone-wallpaper-abstract-portrait-stars-macinmac.jpg' }, { url: 'https://storage.googleapis.com/coverr-main/mp4/Footboys.mp4', type: 'video', duration: 1000 }, { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4', type: 'video'}, { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4', type: 'video' }, 'https://images.unsplash.com/photo-1534856966153-c86d43d53fe0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80']
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -11,7 +15,17 @@ export default function ExperiencePlace(){
             <Sharing />
             <div id="expContainer">
                 <div className="section" id="first-sec">
-                    <div className="left"></div>
+                    <div className="left">
+                        <div>
+                            <Stories 
+                                stories={stories}
+                                defaultInterval={8000}
+                                width={432}
+                                height={768}
+                                loop={true}
+                            />
+                        </div>
+                    </div>
                     <div className="right">
                         <div id="expCategory">
                             [Experience Category]
