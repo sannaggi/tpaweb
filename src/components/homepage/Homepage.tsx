862405c7217dd9d45b2d7e165230dcce316c0969
xyon <albion0211@gmail.com>
2019-07-29 17:24:59 +0700

    added a bit

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -4,6 +4,7 @@ import QuickCard from './QuickCard'
 import RecommmendedPlace from './RecommendedPlace'
 import RecommendedExperience from "./RecommendedExperience";
 import '../../css/homepage/homepage.css'
+import { Link } from "react-router-dom";
 
 function Homepage() {
     return (
@@ -12,8 +13,35 @@ function Homepage() {
                 <QuickCard />
                 <ImageSlider />
             </div>
-            <RecommmendedPlace />
-            <RecommendedExperience />
+            <div className="home-content">
+                <h2>Explore Aivbnb</h2>
+                <div className="container">
+                    <Link to="/places">
+                        <div className="showContainer">
+                            <div className="showImage" style={{backgroundImage: "url('/images/placeHeader.jpg')"}}></div>
+                            <div className="showInfo">
+                                <div>
+                                    <h3>Stays</h3>
+                                    Homes, boutique hotels & more
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link to="/experiences">
+                        <div className="showContainer">
+                            <div className="showImage" style={{backgroundImage: "url('/images/experienceHeader.jpg')"}}></div>
+                            <div className="showInfo">
+                                <div>
+                                    <h3>Experiences</h3>
+                                    Activities hosted by locals
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+                <RecommmendedPlace />
+                <RecommendedExperience />
+            </div>
         </main>
     )
 }
