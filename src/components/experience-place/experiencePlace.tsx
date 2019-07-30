15b17ddccf8be4c487022ae82be351dc4ac325a9
Yumazine <yumazine@gmail.com>
2019-07-30 15:32:36 +0700

    added working pagination

--- src/components/experience-place/experiencePlace.tsx
+++ src/components/experience-place/experiencePlace.tsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import Sharing from "../reusable/sharing";
 import "../../css/experience-place/experiencePlace.css";
 import GoogleMapReact from "google-map-react";
-import InstaStories from "../reusable/InstaStories";
 import Stories from 'react-insta-stories'
+import StarReview from "../reusable/StarReview";
+import ReactPaginate from "react-paginate";
+import GuestComment from "../reusable/GuestComment";
 
-const stories = [{ url: 'https://picsum.photos/1080/1920' }, { url: 'https://fsa.zobj.net/crop.php?r=dyJ08vhfPsUL3UkJ2aFaLo1LK5lhjA_5o6qEmWe7CW6P4bdk5Se2tYqxc8M3tcgYCwKp0IAyf0cmw9yCmOviFYb5JteeZgYClrug_bvSGgQxKGEUjH9H3s7PS9fQa3rpK3DN3nx-qA-mf6XN' }, { url: 'https://media.idownloadblog.com/wp-content/uploads/2016/04/iPhone-wallpaper-abstract-portrait-stars-macinmac.jpg' }, { url: 'https://storage.googleapis.com/coverr-main/mp4/Footboys.mp4', type: 'video', duration: 1000 }, { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4', type: 'video'}, { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4', type: 'video' }, 'https://images.unsplash.com/photo-1534856966153-c86d43d53fe0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80']
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function ExperiencePlace(){
+    var perPage = 5
+    var fetchComment = [
+        {comment: "This place is fucking awesome"},
+        {comment: "This place is fucking awesome"},
+        {comment: "This place is fucking awesome"},
+        {comment: "This place is fucking awesome"},
+        {comment: "This place is fucking awesome"},
+        {comment: "This place is fucking great"},
+        {comment: "This place is fucking great"},
+        {comment: "This place is fucking great"},
+        {comment: "This place is fucking great"},
+        {comment: "This place is fucking great"},
+        {comment: "This place is fucking cool"},
+        {comment: "This place is fucking cool"},
+        {comment: "This place is fucking cool"},
+        {comment: "This place is fucking cool"},
+        {comment: "This place is fucking cool"},
+    ]
+    const [sentComments, setsentComments] = useState(fetchComment.slice(1 * perPage, 1 * perPage + perPage))
+    
+    const stories = [{ url: 'https://picsum.photos/1080/1920' }, { url: 'https://fsa.zobj.net/crop.php?r=dyJ08vhfPsUL3UkJ2aFaLo1LK5lhjA_5o6qEmWe7CW6P4bdk5Se2tYqxc8M3tcgYCwKp0IAyf0cmw9yCmOviFYb5JteeZgYClrug_bvSGgQxKGEUjH9H3s7PS9fQa3rpK3DN3nx-qA-mf6XN' }, { url: 'https://media.idownloadblog.com/wp-content/uploads/2016/04/iPhone-wallpaper-abstract-portrait-stars-macinmac.jpg' }, { url: 'https://storage.googleapis.com/coverr-main/mp4/Footboys.mp4', type: 'video', duration: 1000 }, { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4', type: 'video'}, { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4', type: 'video' }, 'https://images.unsplash.com/photo-1534856966153-c86d43d53fe0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80']
+    var averageRating = 3.65
+    var greenStar = {
+        width: "calc(20px * " + averageRating + ")",
+        overflow: "hidden",
+    }
+
+    function handlePageChange(data){
+        setsentComments(fetchComment.slice(data.selected * perPage, data.selected * perPage + perPage))
+    }
+
     return(
         <div id="expPlace">
             <Sharing />
@@ -20,8 +52,6 @@ export default function ExperiencePlace(){
                             <Stories 
                                 stories={stories}
                                 defaultInterval={8000}
-                                width={432}
-                                height={768}
                                 loop={true}
                             />
                         </div>
@@ -102,12 +132,33 @@ export default function ExperiencePlace(){
                         <img src="https://a0.muscache.com/im/pictures/f4cf0e67-5ccc-44f6-9b06-384aed09ad20.jpg?aki_policy=large" alt=""/>
                     </div>
                 </div>
+                <div className="section" id="section-reviews">
+                    <div className="left">
+                        <div className="section-head">Guest reviews</div>
+                        <div id="rating">{averageRating}<StarReview greenStar={greenStar}/></div>
+                    </div>
+                    <div className="right">
+                        <GuestComment comments={sentComments} viewStar={true}/>
+                        <ReactPaginate
+                            onPageChange={handlePageChange}
+                            pageCount={fetchComment.length / perPage}
+                            pageRangeDisplayed={2}
+                            marginPagesDisplayed={1}
+                            previousLabel={'<'}
+                            nextLabel={'>'}
+                            breakLabel={'...'}
+                            breakClassName={'break-me'}
+                            containerClassName={'pagination'}
+                            activeClassName={'active'}
+                        />
+                    </div>
+                </div>
                 <div className="section">
                     <div className="left">
                         <div className="section-head">Where you'll be</div>
                     </div>
                     <div className="right">
-                    World's 2nd oldest National Park - Royal National Park, NSW Figure 8 Pools Burning Palms Beach Wattamolla Lagoon and Beach Bald Hill Lookout Sea Cliff Bridge on the Grand Pacific Drive
+                        World's 2nd oldest National Park - Royal National Park, NSW Figure 8 Pools Burning Palms Beach Wattamolla Lagoon and Beach Bald Hill Lookout Sea Cliff Bridge on the Grand Pacific Drive
                     </div>
                 </div>
                 <div className="section" style={{ height: '100vh', width: '100%' }}>
--- src/components/place detail/PlaceDetail.tsx
+++ src/components/place detail/PlaceDetail.tsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import "../../css/place detail/placeDetail.css";
 import { setCurrentPlace } from '../../actions/placeActions'
 import Sharing from "../reusable/sharing";
+import StarReview from "../reusable/StarReview";
 
 function PlaceDetail({place, setCurrentPlace, match} : {place:any, setCurrentPlace:any, match:any}){
 
@@ -45,24 +46,7 @@ function PlaceDetail({place, setCurrentPlace, match} : {place:any, setCurrentPla
                     <div className="booking">
                         <div>
                             <div className="price"><div id="price">${place.price}</div><div>/night</div></div>
-                            <div className="reviews">
-                                <div id="star-container">
-                                    <div className="star">
-                                        <img src="data:image/svg+xml,%3Csvg viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg' fill='%23D8D8D8'%3E%3Cpath d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z'/%3E%3C/svg%3E" alt=""/>
-                                        <img src="data:image/svg+xml,%3Csvg viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg' fill='%23D8D8D8'%3E%3Cpath d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z'/%3E%3C/svg%3E" alt=""/>
-                                        <img src="data:image/svg+xml,%3Csvg viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg' fill='%23D8D8D8'%3E%3Cpath d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z'/%3E%3C/svg%3E" alt=""/>
-                                        <img src="data:image/svg+xml,%3Csvg viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg' fill='%23D8D8D8'%3E%3Cpath d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z'/%3E%3C/svg%3E" alt=""/>
-                                        <img src="data:image/svg+xml,%3Csvg viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg' fill='%23D8D8D8'%3E%3Cpath d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z'/%3E%3C/svg%3E" alt=""/>
-                                    </div>
-                                    <div className="star" style={greenStar}>
-                                        <img src="data:image/svg+xml,%3Csvg viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg' fill='%23008489'%3E%3Cpath d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z'/%3E%3C/svg%3E" alt=""/>
-                                        <img src="data:image/svg+xml,%3Csvg viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg' fill='%23008489'%3E%3Cpath d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z'/%3E%3C/svg%3E" alt=""/>
-                                        <img src="data:image/svg+xml,%3Csvg viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg' fill='%23008489'%3E%3Cpath d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z'/%3E%3C/svg%3E" alt=""/>
-                                        <img src="data:image/svg+xml,%3Csvg viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg' fill='%23008489'%3E%3Cpath d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z'/%3E%3C/svg%3E" alt=""/>
-                                        <img src="data:image/svg+xml,%3Csvg viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg' fill='%23008489'%3E%3Cpath d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z'/%3E%3C/svg%3E" alt=""/>
-                                    </div>
-                                </div>
-                            </div>
+                            <StarReview greenStar={greenStar} />
                         </div>
                         <div className="hr"></div>
                         <div id="checkinout">
